2f5cda864b905189087875a95aace907cf829c3f
Brian Bett <bettblake08@hotmail.com>
2018-09-19 19:02:06 +0300

    [Chore #160623190] Changed cc test coverage config

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "Flask-RESTful>=0.3.6",
         "pytest>=3.7.4",
         "coveralls",
-        "pytest-cov>=2.6.0"
+        "pytest-cov>=2.6.0",
+        "codeclimate-test-reporter"
     ]
 )
