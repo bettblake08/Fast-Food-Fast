e91e5e437dfeb009bd1d2a50a000f9c0d36e4c1f
Brian Bett <bettblake08@hotmail.com>
2018-09-22 18:49:26 +0300

    Cleared master branch of unconfirmed code

--- App/Tests/v1/test_fetchOrdersEndpoint.py
+++
@@ -1,7 +0,0 @@
-from App.Api.v1.Resources import Orders
-from App.Tests.v1.test_config import testClient
-
-class TestFetchOrdersEndpoint(object):
-    def test(self, testClient):
-        response = testClient.get('/api/v1/orders')
-        assert response.status_code == 200
--- app.py
+++
@@ -1,5 +0,0 @@
-from App import create_app
-
-if __name__ == "__main__":
-    app = create_app('DEV')
-    app.run()
