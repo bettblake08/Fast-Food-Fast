05a5e7a8d063e8d31b70a34d43bfd7e492596c8b
Brian Bett <bettblake08@hotmail.com>
2018-09-26 23:39:59 +0300

    [Chore #160751750] updated requirements.txt

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,7 @@
 class Config(object):
     SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
     DATABASE_URL = 'postgres://postgres:m21c07s96.A1@localhost:5432/fastfoodfast'
+    SERVER_NAME = 'myappname:5000'
 
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = True
@@ -10,6 +11,7 @@ class Config(object):
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     JWT_SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
+
 
 
 class ProductionConfig(Config):
