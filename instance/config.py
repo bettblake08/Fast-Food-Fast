9263c9ccf84c2f7d814cefc2ea58f0d688337def
Brian Bett <bettblake08@hotmail.com>
2018-09-28 11:48:13 +0300

    [Chore #160812106] Added flask app name in config file

--- instance/config.py
+++ instance/config.py
@@ -4,6 +4,7 @@ class Config(object):
     SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
     DATABASE_URL = 'postgres://postgres:m21c07s96.A1@localhost:5432/fastfoodfast'
     SERVER_NAME = 'localhost.dev:5000'
+    FLASK_APP = "run.py"
 
     JWT_TOKEN_LOCATION = ['headers'] 
     JWT_SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
