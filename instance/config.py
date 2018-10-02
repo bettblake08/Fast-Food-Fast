5845d22c740b2025625931171017be7a2f65a133
Brian Bett <bettblake08@hotmail.com>
2018-10-02 04:24:35 +0300

    [Chore #160812106] updated config file

--- instance/config.py
+++ instance/config.py
@@ -1,14 +1,9 @@
 import os
 
-class Config(object):
-    SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
-    DATABASE_URL = 'postgres://postgres:m21c07s96.A1@localhost:5432/fastfoodfast'
-    SERVER_NAME = 'localhost.dev:5000'
-    
+class Config(object):    
     FLASK_APP = "run.py"
 
     JWT_TOKEN_LOCATION = ['headers'] 
-    JWT_SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
 
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
@@ -25,16 +20,23 @@ class ProductionConfig(Config):
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
 
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
 
 class DevelopmentConfig(Config):
     ENV = "development"
     FLASK_ENV = "development"
     DEBUG = True
+    SERVER_NAME = 'localhost.dev:5000'
 
     DB_HOST = "localhost"
     DB_USER = "postgres"
     DB_PASSWORD = "m21c07s96.A1"
     DB_NAME = "fastfoodfast"
+
+    SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
+    JWT_SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
 
 
 class TestingConfig(Config):
@@ -42,11 +44,16 @@ class TestingConfig(Config):
     TESTING = True
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
+    SERVER_NAME = 'localhost.dev:5000'
     
     DB_HOST = "localhost"
     DB_USER = "postgres"
     DB_PASSWORD = "m21c07s96.A1"
     DB_NAME = "fastfoodfasttest"
+
+    SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
+    JWT_SECRET_KEY = b'\x0c$V\x92\x1b1\x05xp@\xfa\xdc\x94\x87\xc4\x0f'
+
 
 app_config = {
     "DEV": DevelopmentConfig,
