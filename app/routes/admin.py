7b9cb157ca7aa586b56da499ef01df1da2003e3c
Brian Bett <bettblake08@hotmail.com>
2018-10-10 13:51:38 +0300

    [Feature #161113918] added order management route

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -5,3 +5,7 @@ admin_pages = Blueprint('admin_pages', __name__)
 @admin_pages.route("/login")
 def admin_login():
     return render_template('admin/sign_in.html')
+
+@admin_pages.route("/orderManagement")
+def admin_management_page():
+    return render_template('admin/order_management.html')
