26fc88865f896eed70359aea44fa34aff5661758
Brian Bett <bettblake08@hotmail.com>
2018-10-11 15:48:55 +0300

    [Feature #161044684] integrate textinput models

--- resources/js/pages/customer/sign_in.js
+++ resources/js/pages/customer/sign_in.js
@@ -1,10 +1,14 @@
 import {apiV1, webUrl} from "../../abstract/variables";
 import logo from "../../../images/logo2.png";
+import Button from "../../ui/button";
+import TextInput from "../../ui/textInput";
 
 class LoginForm {
 	constructor() {
 		this._state = {
-			"errorMsg": ""
+			errorMsg: "",
+			buttons:[],
+			textInputs:[]
 		};
 	}
 
@@ -17,36 +21,114 @@ class LoginForm {
 	}
 
 	init(){
-		let loginButton = document.querySelector(".login__save button"),
-			loginLogo = document.querySelector(".login__logo");
+		let loginButton = document.querySelector(".login__save"),
+			loginLogo = document.querySelector(".login__logo"),
+			loginInputs = document.querySelectorAll(".login__text");
 
-		loginButton.addEventListener("click", () => {
-			this.loginAuth();
+		let button = new Button({
+			class: "btn_1",
+			textClass: "f_button_1",
+			label: "Sign In",
+			parent: loginForm,
+			status: 0,
+			action: () => {
+				this.loginAuth();
+			}
 		});
+
+		button.init();
+		loginButton.appendChild(button.getButton());
 
 		let logoImg = new Image();
 		logoImg.src = logo;
 		logoImg.alt = "FastFoodFast logo";
 
 		loginLogo.appendChild(logoImg);
+
+		let usernameInput = new TextInput({
+			parent: this,
+			name: "username",
+			label: "Username",
+			placeholder: "Eg. JesseC7",
+			class: "text_input",
+			textClass: "f_input_1",
+			type: "text",
+			status: 0,
+			test: (input) => {
+				if (input.length == 0) {
+					return {
+						status: false,
+						message: "Username field empty."
+					};
+				}
+
+				if (input.length > 30) {
+					return {
+						status: false,
+						message: "Username too long. Please input a username less than 30 characters."
+					};
+				}
+
+				return {
+					status: true
+				};
+			}
+		});
+
+		let passwordInput = new TextInput({
+			parent: this,
+			name: "password",
+			label: "Password",
+			placeholder: "Password",
+			class: "text_input",
+			textClass: "f_input_1",
+			type: "password",
+			status: 0,
+			test: (input) => {
+				if (input.length == 0) {
+					return {
+						status: false,
+						message: "Password field empty."
+					};
+				}
+
+				let passwordCheck = /^((?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$#.!])).{8,16}$/;
+
+				if (!passwordCheck.test(input)) {
+					return {
+						status: false,
+						message: "Password is incorrect." +
+							"Please input a password with 1 upper case, 1 lower case, " +
+							"1 digit and 1 special character including [@$#.!] ."
+					};
+				}
+
+				return {
+					status: true
+				};
+			}
+		});
+
+		usernameInput.init();
+		passwordInput.init();
+
+		loginInputs[0].appendChild(usernameInput.getInput());
+		loginInputs[1].appendChild(passwordInput.getInput());
 	}
 
 	displayError(ERRORDELAY = 3000) {
 		console.log("Displaying error: " + this.state.errorMsg);
-        
-		let state = this.state;
-		const ERRORCOMMENTID = 1;
-            
-		const errorComments = document.querySelectorAll(".errorComment"),
-			errorComment = errorComments.item(ERRORCOMMENTID);
 
-		errorComment.classList.replace("errorComment", "errorComment--active");
+		let state = this.state,
+			errorComment = document.querySelector(".login__error");
+
+		errorComment.classList.replace("errorComment--disabled", "errorComment--active");
 		errorComment.innerHTML = state.errorMsg;
 
 		setTimeout(() => {
 			errorComment.classList.replace(
-				"errorComment--active", 
-				"errorComment"
+				"errorComment--active",
+				"errorComment--disabled"
 			);
 			errorComment.innerHTML = "";
 
@@ -58,55 +140,40 @@ class LoginForm {
 
 	loginAuth() {
 		console.log("Login authentication started");
-        
+
 		const component = this,
-			password = document.querySelector("#password").value,
-			state = component.state,
-			username = document.querySelector("#username").value,
-			usernameMax = 30;
-        
-		if (username.length > usernameMax) {
-			state.errorMsg = "Username too long. Please input username " + 
-                    "less than 30 chars";
-            
-			this.state = state;
-			this.displayError();
+			state = component.state;
+
+		let found = state.textInputs.find((input) => {
+			return input.state.status != 2;
+		});
+
+		if (found != undefined) {
+			found.focus();
 			return;
 		}
 
-		/* 
-		let passwordCheck = new RegExp("((?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$#.!]).{8,16})");
-		
-		if (passwordCheck.exec(password) == null) {
-			state.errorMsg = "Password is incorrect." + 
-                "Please input a password with 1 upper case, 1 lower case, " + 
-                "1 digit and 1 special character including [@$#.!] .";
-
-			this.state = state;
-			this.displayError();
-			return;
-		} 
-		*/
-
-		fetch(`${apiV1}/auth/login`, { 
+		fetch(`${apiV1}/auth/login`, {
 			body: JSON.stringify({
-				password,
-				username                
+				password:state.textInputs[1].getInputValue(),
+				username: state.textInputs[0].getInputValue()
 			}),
 			headers: {
 				"Content-Type": "application/json"
 			},
-			method:"POST"         
+			method: "POST"
 		}).then((response) => {
 
-			switch(response.status){
-			case 200:{  
-				return response.json();    
+			switch (response.status) {
+			case 200:
+			{
+				return response.json();
 			}
 			case 400:
 			case 401:
-			case 404: {
-				response.json().then((result)=>{
+			case 404:
+			{
+				response.json().then((result) => {
 					state.errorMsg = result.message;
 					this.state = state;
 					this.displayError();
@@ -116,14 +183,13 @@ class LoginForm {
 			}
 		}).then((response) => {
 			console.log("Login Successful!");
-            
-			const loginCredentials = {
+
+			localStorage.setItem("tokens", JSON.stringify({
 				access_token: response.access_token,
 				refresh_token: response.refresh_token
-			};
+			}));
 
-			localStorage.setItem("tokens", JSON.stringify(loginCredentials));
-			window.location.href = webUrl + "/customer/order";
+			window.location.href = webUrl + "/admin/orderManagement";
 		});
 	}
 }
