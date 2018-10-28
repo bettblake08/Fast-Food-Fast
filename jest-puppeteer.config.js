38bd17e2f8c59afc666866127c8c9f1b5fcb9a4b
Brian Bett <bettblake08@hotmail.com>
2018-10-28 08:13:04 +0300

    [Chore #161360240] updated jest puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -9,3 +9,19 @@ module.exports = {
 	},
 	browserContext: "default",
 };
+
+/* 
+module.exports = {
+	launch: {
+		dumpio: false,
+		headless: false,
+		slowMo:100,
+		defaultViewPort: {
+			width: 1920,
+			height: 1080,
+			isLandscape:true
+		}
+	},
+	browserContext: "default",
+};
+ */
