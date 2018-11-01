a08413267ee0acb50aea326f9ea5e657ac74369f
Brian Bett <bettblake08@hotmail.com>
2018-11-01 08:06:09 +0300

    [Chore #161360247] fixed admin login page test bug

--- tests/__tests__/pages/admin/sign_in.js
+++ tests/__tests__/pages/admin/sign_in.js
@@ -118,20 +118,23 @@ describe("Admin Sign In Page: ", () => {
 	},10000);
 
 	it("Test login using valid inputs", async () => {
-		await page.waitFor(5000);
+		await page.screenshot({
+			path: `${SCR_PATH}5-1.jpg`
+		});
+
 		await page.type(loginUsernameInput, user.name);
 		await page.type(loginPasswordInput, user.password);
-		await page.waitFor(1000);
+		await page.waitFor(5000);
 		await page.click(loginButton);
 
 		await page.screenshot({
-			path: `${SCR_PATH}5-1.jpg`
+			path: `${SCR_PATH}5-2.jpg`
 		});
 
 		await page.waitFor(3000);
 
 		await page.screenshot({
-			path: `${SCR_PATH}5-2.jpg`
+			path: `${SCR_PATH}5-3.jpg`
 		});
 
 		const newPageTitle = await page.title();
